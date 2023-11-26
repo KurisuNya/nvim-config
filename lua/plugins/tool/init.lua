bbe3ec4e733bf522ecbe50bfeb1596e84e22f057
KurisuNya <cuizhu1216@gmail.com>
2023-11-27 00:46:50 +0800

    fix: small fix

--- lua/plugins/tool/init.lua
+++ lua/plugins/tool/init.lua
@@ -191,7 +191,7 @@ local M = {
 			},
 		},
 		init = function()
-			vim.g.table_mode_kap_prefix = "<Bar>"
+			vim.g.table_mode_map_prefix = "<Bar>"
 			vim.g.table_mode_toggle_map = "<Bar>"
 			vim.g.table_mode_tableize_map = "<leader><Bar>"
 			vim.g.table_mode_corner = "|"
