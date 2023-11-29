232010be312dbc1b4bd098bdb4ded63195c3896c
KurisuNya <cuizhu1216@gmail.com>
2023-11-29 16:00:07 +0800

    feat: 优化 cmp 按键

--- lua/core/keymaps/nvim-cmp.lua
+++ lua/core/keymaps/nvim-cmp.lua
@@ -1,5 +1,10 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 local M = {}
 M.keymap_list = {
 	["<Tab>"] = cmp.mapping(function(fallback)
@@ -21,15 +26,17 @@ M.keymap_list = {
 		end
 	end, { "i", "s" }),
 	["<CR>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			if cmp.get_selected_entry() then
-				cmp.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = false,
-				})
-			else
-				cmp.abort()
-			end
+		if not cmp.visible() then
+			fallback()
+			return
+		end
+		if cmp.get_selected_entry() then
+			cmp.confirm({
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = false,
+			})
+		elseif has_words_before() then
+			cmp.abort()
 		else
 			fallback()
 		end
