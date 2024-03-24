f0d0681ed7adb11d807584b34930318b82a50725
KurisuNya <cuizhu1216@gmail.com>
2024-03-25 03:29:26 +0800

    fix: small fix

--- lua/plugins/lsp/nvim-dap-python.lua
+++ lua/plugins/lsp/nvim-dap-python.lua
@@ -3,9 +3,9 @@ M.config = function()
 	local path = require("mason-registry").get_package("debugpy"):get_install_path()
 
 	if vim.loop.os_uname().sysname == "Windows_NT" then
-		require("dap-python").setup(path .. "/Scripts/python.exe")
+		require("dap-python").setup(path .. "/venv/Scripts/python.exe")
 	else
-		require("dap-python").setup(path .. "/bin/python")
+		require("dap-python").setup(path .. "/venv/bin/python")
 	end
 	table.insert(require("dap").configurations.python, {
 		type = "python",
