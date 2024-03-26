8fc6a9b9e53452d5024a2b6db754ff5cdff5187f
KurisuNya <cuizhu1216@gmail.com>
2024-03-27 01:42:52 +0800

    feat: change colorscheme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -13,6 +13,11 @@ M.colorscheme_plugin = {
 				keywords = { italic = true },
 				-- floats = "normal",
 			},
+			on_highlights = function(hl, c)
+				hl.TelescopePromptNormal = hl.TelescopeNormal
+				hl.TelescopePromptBorder = hl.TelescopeBorder
+				hl.TelescopePromptTitle = hl.TelescopeTitle
+			end,
 		})
 		vim.cmd.colorscheme("tokyonight-storm")
 		vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { link = "Visual" })
