b1e14b95511efc6d55f63f257812e918226f9675
KurisuNya <cuizhu1216@gmail.com>
2024-07-13 15:05:49 +0800

    feat: fix which key

--- lua/plugins/tool/init.lua
+++ lua/plugins/tool/init.lua
@@ -174,11 +174,7 @@ local M = {
 	},
 	{
 		"folke/which-key.nvim",
-		init = function()
-			vim.opt.timeout = true
-			vim.opt.timeoutlen = 1000
-		end,
-		opts = {},
+		opts = { delay = 1000 },
 		event = "VeryLazy",
 	},
 	{
