9a9806709f49251e152cd58cbbe3904e42aa566b
KurisuNya <cuizhu1216@gmail.com>
2025-07-20 03:03:29 +0800

    feat(notify): add mason install notify

--- lua/plugins/language/mason.lua
+++ lua/plugins/language/mason.lua
@@ -7,10 +7,19 @@ return {
 		require("mason").setup(opts)
 		local tbl = Utils.tbl_filter_same(PluginVars.mason_ensure_installed)
 		local mr = require("mason-registry")
+		mr:on("package:install:success", function()
+			vim.defer_fn(function()
+				require("lazy.core.handler.event").trigger({
+					event = "FileType",
+					buf = vim.api.nvim_get_current_buf(),
+				})
+			end, 100)
+		end)
 		mr.refresh(function()
 			for _, tool in ipairs(tbl) do
 				local p = mr.get_package(tool)
 				if not p:is_installed() then
+					vim.notify("Installing " .. tool, vim.log.levels.INFO, { title = "Mason" })
 					p:install()
 				end
 			end
