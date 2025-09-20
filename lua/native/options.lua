e14643ef2cc818d94fd8ca78bd766b4989862c55
KurisuNya <cuizhu1216@gmail.com>
2025-09-20 15:01:19 +0800

    fix fold

--- lua/native/options.lua
+++ lua/native/options.lua
@@ -73,7 +73,6 @@ _G.highlighted_foldtext = function()
 	local line = vim.fn.getline(vim.v.foldstart)
 	local len = vim.v.foldend - vim.v.foldstart
 	local suffix = "  " .. len .. (len > 1 and " lines" or " line") .. " folded "
-
 	local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
 	local parser = vim.treesitter.get_parser(0, lang, { error = false })
 	if not parser then
@@ -91,22 +90,28 @@ _G.highlighted_foldtext = function()
 	local pos = vim.v.foldstart
 	local tree = parser:parse({ pos - 1, pos })[1]
 	for id, node, _ in query:iter_captures(tree:root(), 0, pos - 1, pos) do
-		local name = query.captures[id]
 		local start_row, start_col, end_row, end_col = node:range()
-		if start_row == pos - 1 and end_row == pos - 1 then
-			local range = { start_col, end_col }
-			if start_col > line_pos then
-				table.insert(result, { line:sub(line_pos + 1, start_col), "Folded" })
-			end
-			line_pos = end_col
-			local text = vim.treesitter.get_node_text(node, 0)
-			if prev_range ~= nil and range[1] == prev_range[1] and range[2] == prev_range[2] then
-				result[#result] = { text, "@" .. name }
-			else
-				table.insert(result, { text, "@" .. name })
-			end
-			prev_range = range
+		if start_row ~= pos - 1 or end_row ~= pos - 1 then
+			goto continue
 		end
+		local range = { start_col, end_col }
+		if start_col > line_pos then
+			table.insert(result, { line:sub(line_pos + 1, start_col), "Folded" })
+		end
+		local name = query.captures[id]
+		local text = vim.treesitter.get_node_text(node, 0)
+		if prev_range == nil then
+			table.insert(result, { text, "@" .. name })
+		elseif range[1] == prev_range[1] and range[2] == prev_range[2] then
+			result[#result] = { text, "@" .. name }
+		elseif start_col < line_pos then
+			goto continue
+		else
+			table.insert(result, { text, "@" .. name })
+		end
+		prev_range = range
+		line_pos = end_col
+		::continue::
 	end
 	return vim.list_extend(result, { { suffix, "Folded" } })
 end
