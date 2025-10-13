26d5eef51698fd87ecbdca0b33c209944f4f1931
KurisuNya <cuizhu1216@gmail.com>
2025-10-13 15:49:03 +0800

    fix(cspell): add cspell to lualine_hidden_lsp

--- lua/plugins/language/lang/misc/cspell.lua
+++ lua/plugins/language/lang/misc/cspell.lua
@@ -1,4 +1,5 @@
 PluginVars.insert(PluginVars.mason_ensure_installed, "cspell-lsp")
+PluginVars.insert(PluginVars.lualine_hidden_lsp, "cspell_ls")
 PluginVars.insert(PluginVars.lsp_config, function()
 	vim.lsp.enable("cspell_ls")
 end)
